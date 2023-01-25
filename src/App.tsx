1bbc5e48860bed3435765ee20e9a719b3dc3ed2a
Wali Queiroz <wsantos077@gmail.com>
2023-01-25 16:42:24 -0300

    wip: create Header, SummaryTable e HabitDay components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
-import Habit from './components/Habit'
 
-function App() {
+export function App() {
   return (
-    <div className='row-auto'>
-      <div className='col-span-2'>
-        <Habit completed={4} />
-      </div>
-      <div className='col-span-6'>
-        <Habit completed={4} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
       </div>
     </div>
   )
 }
-
-export default App
---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,12 @@
+interface HabitDayProps {
+}
+
+function HabitDay(props: HabitDayProps) {
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
+
+        </div>
+    )
+}
+
+export default HabitDay
